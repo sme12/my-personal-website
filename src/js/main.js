57bc777d0306de68b1398108a88d7e84b5ba00ea
sme12 <smetanoioioi@gmail.com>
2016-06-08 00:13:10 +0300

    Добавил Обо мне и штучки всякие

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,18 @@ $('.hamb').click( function() {
   $(this).toggleClass('is-open');
   $('nav').toggleClass('is-open');
 } );
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
