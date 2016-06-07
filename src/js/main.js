5201846e8f489be60248cb53ca42189db4af2651
sme12 <smetanoioioi@gmail.com>
2016-06-07 16:46:13 +0300

    Добавил сетку, сделал хедер и навигацию

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
-jQuery('.coming-soon').imagesLoaded( { background: true }, function() {
-  $('.dark').fadeOut('1000');
-  $('#stroke').attr("class", "stroke stroke-anim")
-  $('#fill').attr("class", "fill fill-anim")
-  $('.text').addClass('text-anim');
-  console.log('Hey!');
+$('header').imagesLoaded( { background: true }, function() {
+  $('.cover').fadeOut('1000');
+  // $('#stroke').attr("class", "stroke stroke-anim")
+  // $('#fill').attr("class", "fill fill-anim")
+  // $('.text').addClass('text-anim');
+  // console.log('Hey!');
 });
+
+$('.hamb').click( function() {
+  $(this).toggleClass('is-open');
+  $('nav').toggleClass('is-open');
+} );
